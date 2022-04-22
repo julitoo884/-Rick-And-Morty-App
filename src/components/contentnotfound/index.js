f920211db625ac63f4c45d8ec0c059b358ab2b31
Julio Rangel <51465954+julitoo884@users.noreply.github.com>
2022-04-22 12:38:10 -0400

    Width fix

--- src/components/contentnotfound/index.js
+++ src/components/contentnotfound/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const ContainerVerticalAlign = styled.div`
     display : flex;
     width : 100%;
+    max-width : 100vw;
     flex-direction : column;
     align-items : center;
     justify-content : center;
